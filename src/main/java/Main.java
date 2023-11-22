ce261a18b0f9a91e19a7221dd06a513c3483c332
LebedevaMaria <144733968+LebedevaMaria@users.noreply.github.com>
2023-11-22 22:38:09 +0300

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner input = new Scanner(System.in);
+    System.out.println("В файлах должна быть информация на английском языке либо фильтрация будет происходить с учетом регистра");
+    System.out.print("Введите путm первого файла: ");
+    String firstFile = input.nextLine();
+    System.out.print("Введите путь второго файла: ");
+    String secondFile = input.nextLine();
+    System.out.print("Введите путь третьего файла: ");
+    String newFile = input.nextLine();
+    FilterFile filterFile = new FilterFile(firstFile, secondFile, newFile);
+    filterFile.filterFileAndRecord();
+  }
+}
